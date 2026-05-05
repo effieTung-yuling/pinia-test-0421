a8d5104e8a5cdd51b7be8ad6d2a29b87ce65c131
MSI\Effie <dongyuling@semi-tech.com>
2026-05-05 16:01:44 +0800

    Reapply "feat: integrate Axios for API calls and refactor data fetching in stores"
    This reverts commit a142e98df4c68c273b08205c9e7902dfbbcdef86.

--- src/stores/productsData.js
+++ src/stores/productsData.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { fetchProducts as fetchProductsAPI } from "@/api/products";
 
 export const useProductsStore = defineStore("products", () => {
   const items = ref([]);
@@ -8,12 +9,8 @@ export const useProductsStore = defineStore("products", () => {
   async function fetchProducts() {
     isLoading.value = true; // 開始抓取時設為 true
     try {
-      const response = await fetch(
-        "https://southerntravel.onrender.com/api/Products",
-      );
-      if (!response.ok) throw new Error("網路回應不正常");
-      const data = await response.json();
-      items.value = data;
+      const response = await fetchProductsAPI();
+      items.value = response.data;
     } catch (error) {
       console.error("API 抓取失敗:", error);
     } finally {
